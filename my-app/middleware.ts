f8b0ed6e4dd77a31632dece817eca27ed5e28eaf
Naum.Bebekoski-FE18 <bebekoski123@gmail.com>
2024-04-15 15:20:58 +0200

    middleware update

--- my-app/middleware.ts
+++ my-app/middleware.ts
@@ -18,23 +18,47 @@
 // };
 
 
+// import { authMiddleware } from "@clerk/nextjs";
+
+// // const protectedRoute = createRouteMatcher([
+// //   '/',
+// //   '/upcoming',
+// //   '/meeting(.*)',
+// //   '/previous',
+// //   '/recordings',
+// //   '/personal-room',
+// // ]);
+
+// // export default clerkMiddleware((auth, req) => {
+// //   if (protectedRoute(req)) auth().protect();
+// // });
+
+// export default authMiddleware();
+
+// export const config = {
+//   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
+
+
+
 import { authMiddleware } from "@clerk/nextjs";
+// import createMiddleware from "next-intl/middleware";
 
-// const protectedRoute = createRouteMatcher([
-//   '/',
-//   '/upcoming',
-//   '/meeting(.*)',
-//   '/previous',
-//   '/recordings',
-//   '/personal-room',
-// ]);
+// const intlMiddleware = createMiddleware({
+//   locales: ["en", "zh"],
+//   defaultLocale: "en",
+// });
 
-// export default clerkMiddleware((auth, req) => {
-//   if (protectedRoute(req)) auth().protect();
+// export default authMiddleware({
+//   beforeAuth: (req) => {
+//     return intlMiddleware(req);
+//   },
+
+//   publicRoutes: ["/"],
 // });
 
 export default authMiddleware();
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
