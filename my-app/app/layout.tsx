79f431d7350ab129f3a04e59c7ef534b6a8c284b
Naum.Bebekoski-FE18 <bebekoski123@gmail.com>
2024-04-16 14:52:50 +0200

    Stream Create Meeting

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 
+import { Toaster } from "@/components/ui/toaster";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -20,18 +22,21 @@ export default function RootLayout({
       <ClerkProvider
         appearance={{
           layout: {
-            logoImageUrl: '/icons/logo.svg'
+            logoImageUrl: "/icons/logo.svg",
           },
           variables: {
-            colorText: '#fff',
+            colorText: "#fff",
             colorPrimary: "#0E78F9",
-            colorBackground: '#1c1f2e',
-            colorInputBackground: '#252a41',
-            colorInputText: '#fff',
+            colorBackground: "#1c1f2e",
+            colorInputBackground: "#252a41",
+            colorInputText: "#fff",
           },
         }}
       >
-        <body className={`${inter.className} bg-dark-2`}>{children}</body>
+        <body className={`${inter.className} bg-dark-2`}>
+          {children}
+          <Toaster />
+        </body>
       </ClerkProvider>
     </html>
   );
--- my-app/components/MeetingTypeList.tsx
+++ my-app/components/MeetingTypeList.tsx
@@ -2,17 +2,72 @@
 
 import React, { useState } from "react";
 import HomeCard from "./HomeCard";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import MeetingModal from "./MeetingModal";
+import { useUser } from "@clerk/nextjs";
+import { useStreamVideoClient } from "@stream-io/video-react-sdk";
+import { Call } from "@stream-io/video-react-sdk";
+import { Toaster } from "@/components/ui/toaster";
+import { useToast } from "./ui/use-toast";
+
 
 const MeetingTypeList = () => {
-  // const router = useRouter();
+  const route = useRouter();
 
   const [meetingState, setMeetingState] = useState<
     "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
   >();
+  const { user } = useUser();
+  const client = useStreamVideoClient();
+  const [values, setValues] = useState({
+    dateTime: new Date(),
+    description: '',
+    link: '',
+  });
 
-  const createMeeting = () => {};
+  const [callDetails, setCallDetails] = useState<Call>();
+  const {toast} = useToast();
+
+  const createMeeting = async () => {
+    if (!client || !user) return;
+
+    try {
+      if(!values.dateTime) {
+        toast({
+          title: "Please select a date and a time",
+        })
+        return;
+      }
+      const id = crypto.randomUUID(); // Random id
+      const call = client.call('default', id);
+      if(!call) throw new Error('Failed to create call')
+
+      const startsAt = values.dateTime.toISOString() || new Date(Date.now()).toISOString();
+      const description = values.description || 'Instant meeting';
+
+      await call.getOrCreate({
+        data: {
+          starts_at: startsAt,
+          custom: {
+            description
+          }
+        }
+      })
+      setCallDetails(call);
+
+      if(!values.description) {
+        route.push(`/meeting/${call.id}`);
+      }
+      toast({
+        title: "Meeting Created",
+      })
+    } catch (error) {
+      console.log(error);
+      toast({
+        title: "Failed to create meeting",
+      })
+    }
+  };
 
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
@@ -42,9 +97,8 @@ const MeetingTypeList = () => {
         title="View Recordings"
         description="Meeting Recordings"
         className="bg-yellow-1"
-        handleClick={() => {}}
+        handleClick={() => route.push('/recordings')}
       />
-
 
       <MeetingModal
         isOpen={meetingState === "isInstantMeeting"}
