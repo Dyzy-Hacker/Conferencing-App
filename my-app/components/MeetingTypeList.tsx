fd1b63cf2b875cdb7b1e50485da7e6e7938ae6df
Naum.Bebekoski-FE18 <bebekoski123@gmail.com>
2024-04-16 11:03:48 +0200

    Reusable Modal

--- my-app/components/MeetingTypeList.tsx
+++ my-app/components/MeetingTypeList.tsx
@@ -3,13 +3,16 @@
 import React, { useState } from "react";
 import HomeCard from "./HomeCard";
 import { useRouter } from "next/router";
+import MeetingModal from "./MeetingModal";
 
 const MeetingTypeList = () => {
   // const router = useRouter();
 
   const [meetingState, setMeetingState] = useState<
-    "isScheduleMeeting" | "isJoinMeeting" | "isInstantMeeting" | undefined
+    "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
   >();
+
+  const createMeeting = () => {};
 
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
@@ -17,29 +20,38 @@ const MeetingTypeList = () => {
         img="/icons/add-meeting.svg"
         title="New Meeting"
         description="Start an instant meeting"
-        handleClick={() => setMeetingState("isJoinMeeting")}
+        handleClick={() => setMeetingState("isInstantMeeting")}
         className="bg-orange-1"
       />
       <HomeCard
         img="/icons/join-meeting.svg"
         title="Join Meeting"
         description="via invitation link"
-        handleClick={() => setMeetingState("isInstantMeeting")}
         className="bg-blue-1"
+        handleClick={() => setMeetingState("isJoiningMeeting")}
       />
       <HomeCard
         img="/icons/schedule.svg"
         title="Schedule Meeting"
         description="Plan your meeting"
-        handleClick={() => setMeetingState("isScheduleMeeting")}
         className="bg-purple-1"
+        handleClick={() => setMeetingState("isScheduleMeeting")}
       />
       <HomeCard
         img="/icons/recordings.svg"
         title="View Recordings"
-        description="Check out your recordings"
-        handleClick={() => {}}
+        description="Meeting Recordings"
         className="bg-yellow-1"
+        handleClick={() => {}}
+      />
+
+
+      <MeetingModal
+        isOpen={meetingState === "isInstantMeeting"}
+        onClose={() => setMeetingState(undefined)}
+        title="Start an Instant Meeting"
+        buttonText="Start Meeting"
+        handleClick={createMeeting}
       />
     </section>
   );
